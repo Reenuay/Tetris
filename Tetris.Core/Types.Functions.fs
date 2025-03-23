8baa353d5df13a109e8ba32ff86ae1b119f7de5b
Rustam Azizov <reenuay777@gmail.com>
2025-03-23 23:37:24 +0400

    Refactor: simplify GameBoard type and add Tetromino-related types
    Refactor the GameBoard type to use a single-case union for better clarity and maintainability. Add new types for Tetromino rotation, cell, and figure, and implement a function to convert TetrominoType to TetrominoFigure. Apply [<RequireQualifiedAccess>] to relevant types and modules for better encapsulation.

--- Tetris.Core/Types.Functions.fs
+++ Tetris.Core/Types.Functions.fs
@@ -7,6 +7,7 @@ open FSharpPlus.Data
 /// <summary>
 /// Contains functions for creating and manipulating game boards.
 /// </summary>
+[<RequireQualifiedAccess>]
 module GameBoard =
     /// <summary>
     /// Minimal width of a game board.
@@ -24,14 +25,28 @@ module GameBoard =
     /// <param name="width">The width of the game board.</param>
     /// <param name="height">The height of the game board.</param>
     let private createCells<'a> (width: int) (height: int) =
-        { Cells = konst BoardCell<'a>.Empty |> List.init width |> konst |> List.init height }
+        konst BoardCell<'a>.Empty
+        |> List.init width
+        |> konst
+        |> List.init height
+        |> GameBoard
 
+    /// <summary>
+    /// Validates the width of a game board.
+    /// </summary>
+    /// <param name="width">The width of the game board.</param>
+    /// <returns>A list of errors if the width is invalid.</returns>
     let private validateWidth width =
         if width < minWidth then
             Failure [ WidthTooSmall(minWidth, width) ]
         else
             Success width
 
+    /// <summary>
+    /// Validates the height of a game board.
+    /// </summary>
+    /// <param name="height">The height of the game board.</param>
+    /// <returns>A list of errors if the height is invalid.</returns>
     let private validateHeight height =
         if height < minHeight then
             Failure [ HeightTooSmall(minHeight, height) ]
@@ -53,3 +68,23 @@ module GameBoard =
     /// </summary>
     let standard<'a> =
         tryCreate<'a> 10 20 |> Validation.defaultWith (fun _ -> failwith "Unreachable")
+
+/// <summary>
+/// Contains functions for working with tetromino types.
+/// </summary>
+[<RequireQualifiedAccess>]
+module TetrominoType =
+    /// <summary>
+    /// Creates a figure from the given tetromino type.
+    /// </summary>
+    /// <param name="type'">The tetromino type.</param>
+    let toFigure type' =
+        match type' with
+        | TetrominoType.I -> [ [ X ]; [ X ]; [ X ]; [ X ] ]
+        | TetrominoType.J -> [ [ O; X ]; [ O; X ]; [ X; X ] ]
+        | TetrominoType.L -> [ [ X; O ]; [ X; O ]; [ X; X ] ]
+        | TetrominoType.O -> [ [ X; X ]; [ X; X ] ]
+        | TetrominoType.S -> [ [ O; X; X ]; [ X; X; O ] ]
+        | TetrominoType.T -> [ [ X; X; X ]; [ O; X; O ] ]
+        | TetrominoType.Z -> [ [ X; X; O ]; [ O; X; X ] ]
+        |> TetrominoFigure
--- Tetris.Core/Types.fs
+++ Tetris.Core/Types.fs
@@ -6,6 +6,7 @@
 /// Additional data like color can be stored in the occupied cell.
 /// </summary>
 /// <typeparam name="'cellData">The type of data stored in the occupied cell.</typeparam>
+[<RequireQualifiedAccess>]
 type BoardCell<'cellData> =
     | Empty
     | Occupied of 'cellData
@@ -14,13 +15,12 @@ type BoardCell<'cellData> =
 /// Represents a 2D game board that consists of cells.
 /// </summary>
 /// <typeparam name="'cellData">The type of addtitional data that can be stored in each occupied cell of the board.</typeparam>
-type GameBoard<'cellData> =
-    private
-        { Cells: BoardCell<'cellData> list list }
+type GameBoard<'cellData> = private GameBoard of BoardCell<'cellData> list list
 
 /// <summary>
 /// Represents the tetrominoes that can be used in the game.
 /// </summary>
+[<RequireQualifiedAccess>]
 type TetrominoType =
     | I
     | J
@@ -29,3 +29,35 @@ type TetrominoType =
     | S
     | T
     | Z
+
+/// <summary>
+/// Represents the rotation of a tetromino.
+/// </summary>
+[<RequireQualifiedAccess>]
+type TetrominoRotation =
+    /// <summary>
+    /// Represents the clockwise rotation of a tetromino.
+    /// </summary>
+    | Clockwise
+    /// <summary>
+    /// Represents the counterclockwise rotation of a tetromino.
+    /// </summary>
+    | CounterClockwise
+
+/// <summary>
+/// Represents a single cell in the tetromino that can be either empty or occupied.
+/// </summary>
+type TetrominoCell =
+    /// <summary>
+    /// Represents an occupied cell in the tetromino.
+    /// </summary>
+    | X
+    /// <summary>
+    /// Represents an empty cell in the tetromino.
+    /// </summary>
+    | O
+
+/// <summary>
+/// Represents a tetromino figure that consists of cells.
+/// </summary>
+type TetrominoFigure = private TetrominoFigure of TetrominoCell list list
