f392f31f667a4ca0eda3fcb3cf27044e13ad1a5b
Rustam Azizov <reenuay777@gmail.com>
2025-03-31 04:04:02 +0400

    Refactor(test): simplify test method names in Grid.fs
    Remove redundant "Grid." prefix from test method names to improve readability and maintain consistency

--- Tetris.Core.Test/Grid.fs
+++ Tetris.Core.Test/Grid.fs
@@ -6,34 +6,34 @@ open FsUnitTyped
 
 
 [<Fact>]
-let ``Grid.tryCreate fails when tiles is null`` () =
+let ``tryCreate fails when tiles is null`` () =
     let tiles = null
 
     Grid.tryCreate tiles |> Result.shouldBeError [ Grid.NullTiles ]
 
 [<Fact>]
-let ``Grid.tryCreate fails when width is too small`` () =
+let ``tryCreate fails when width is too small`` () =
     let tiles = Array2D.create Grid.minHeight 0 Tile.Empty
 
     Grid.tryCreate tiles
     |> Result.shouldBeError [ Grid.WidthTooSmall(Grid.minWidth, 0) ]
 
 [<Fact>]
-let ``Grid.tryCreate fails when height is too small`` () =
+let ``tryCreate fails when height is too small`` () =
     let tiles = Array2D.create 0 Grid.minWidth Tile.Empty
 
     Grid.tryCreate tiles
     |> Result.shouldBeError [ Grid.HeightTooSmall(Grid.minHeight, 0) ]
 
 [<Fact>]
-let ``Grid.tryCreate fails when width and height are too small`` () =
+let ``tryCreate fails when width and height are too small`` () =
     let tiles = Array2D.create 0 0 Tile.Empty
 
     Grid.tryCreate tiles
     |> Result.shouldBeError [ Grid.WidthTooSmall(Grid.minWidth, 0); Grid.HeightTooSmall(Grid.minHeight, 0) ]
 
 [<Fact>]
-let ``Grid.tryCreate succeeds when given tiles are valid`` () =
+let ``tryCreate succeeds when given tiles are valid`` () =
     let tiles = Array2D.create Grid.minHeight Grid.minWidth Tile.Empty
 
     Grid.tryCreate tiles |> Result.shouldBeOk
